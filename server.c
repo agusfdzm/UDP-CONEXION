08ba86b6536273a0068ecb8d2772104fee83c31c
agusfdzm <agustinfernandezmendoza2004@gmail.com>
2025-10-15 19:01:51 +0200

    Creación del socket

--- server.c
+++ server.c
@@ -1,9 +1,31 @@
-#include <stdio.h> // Imprimir cosas en la terminal
+// para saber mas sobre las librerías leer obsidian
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        printf("Uso: %s <puerto>\n", argv[0]);
+        exit(1);
+    }
+
+    int port = atoi(argv[1]);
+
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+        perror("socket");
+        exit(1);
+    }
+
+    struct sockaddr_in si_me;
+    memset(&si_me, 0, sizeof(si_me));
+
+    si_me.sin_family = AF_INET; // IP
+    si_me.sin_port = htons(port);
+    si_me.sin_addr.s_addr = htonl(INADDR_ANY); // Cualquier interfaz
+}
